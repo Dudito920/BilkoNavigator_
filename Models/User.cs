80d540421c3cdee92ccf967e2d3dd1d992293a84
Николай Дудов <nd26149582@edu.mon.bg>
2026-01-22 10:15:29 +0200

    добавих код в юзер

--- Models/User.cs
+++ Models/User.cs
@@ -5,5 +5,6 @@ namespace BilkoNavigator_.Models
     public class User : IdentityUser
     {
         public string UserStatus { get; set; } = "Active"; // Set default value
+        public int password { get; set; };
     }
 }
