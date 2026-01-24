9bef89f8c2fdfa004629a83943ef3f1d3de0a1d0
Neli <ppaneva.neli@gmail.com>
2026-01-24 21:54:26 +0200

    Направена е функционалност за добавяне на изображенияя, редактиране, детайли и изтриване на билка.

--- Controllers/HerbsController.cs
+++ Controllers/HerbsController.cs
@@ -29,11 +29,7 @@ namespace BilkoNavigator_.Controllers
             _environment = environment;
         }
 
-        // GET: Herbs
-        //public async Task<IActionResult> Index()
-        //{
-        //    return View(await _context.Herbs.ToListAsync());
-        //}
+        
 
         // GET: Herbs/Details/5
         public async Task<IActionResult> Details(int? id)
@@ -44,6 +40,7 @@ namespace BilkoNavigator_.Controllers
             }
 
             var herb = await _context.Herbs
+                .Include(h => h.Image)
                 .FirstOrDefaultAsync(m => m.Id == id);
             if (herb == null)
             {
@@ -59,66 +56,12 @@ namespace BilkoNavigator_.Controllers
             return View();
         }
 
-        // POST: Herbs/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        ////[HttpPost]
-        ////[ValidateAntiForgeryToken]
-        ////public async Task<IActionResult> Create([Bind("Id,PopularName,LatinName,DialectNames,Aroma,Taste,Habitat,Season,IsPoisonous,IsProtected,UsedPart,Benefits,Description")] Herb herb, IFormFile Image)
-        ////{
-
-        ////    var imagesDir = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images");
-        ////    if (!Directory.Exists(imagesDir))
-        ////        Directory.CreateDirectory(imagesDir);
-
-        ////    if (ModelState.IsValid)
-        ////    {
-        ////        if (Image != null && Image.Length > 0)
-        ////        {
-
-        ////            var fileName = $"{Guid.NewGuid()}_{Path.GetFileName(Image.FileName)}";
-        ////            var imagePath = Path.Combine("wwwroot/images", fileName);
-
-        ////            //var imagePath = Path.Combine("wwwroot/images", Image.FileName);
-
-        ////            using (var stream = new FileStream(imagePath, FileMode.Create))
-        ////            {
-        ////                await Image.CopyToAsync(stream);
-        ////            }
-
-        ////            herb.Image = new HerbImage
-        ////            {
-        ////                ImagePath = $"/images/{fileName}",
-        ////                UploadedOn = DateTime.Now
-        ////            };
-        ////        }
-
-        ////        _context.Herbs.Add(herb);
-        ////        await _context.SaveChangesAsync();
-        ////        return RedirectToAction(nameof(Index));
-        ////    }
-
-        ////    // Log validation errors for debugging
-        ////    foreach (var error in ModelState.Values.SelectMany(v => v.Errors))
-        ////    {
-        ////        Console.WriteLine($"Validation Error: {error.ErrorMessage}");
-        ////    }
-
-        ////    return View(herb);
-        ////}
-
+       
         // POST: Herbs/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create(Herb herb)
         {
-//            Console.WriteLine(">>> POST CREATE HIT <<<"); 
-//            Console.WriteLine(
-//    herb.ImageFile == null
-//        ? "❌ ImageFile = NULL"
-//        : $"✅ ImageFile = {herb.ImageFile.FileName}, {herb.ImageFile.Length} bytes"
-//);
-
             // Логване на ModelState грешки
             foreach (var state in ModelState)
             {
@@ -164,11 +107,7 @@ namespace BilkoNavigator_.Controllers
         }
 
         // GET: Herbs/Index
-        //public async Task<IActionResult> Index()
-        //{
-        //    var herbs = await _context.Herbs.Include(h => h.Image).ToListAsync();
-        //    return View(herbs);
-        //}
+       
         public async Task<IActionResult> Index()
         {
             var herbs = await _context.Herbs
@@ -233,95 +172,8 @@ namespace BilkoNavigator_.Controllers
             return RedirectToAction(nameof(Index));
         }
 
-        // POST: Herbs/Delete
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Delete(int id)
-        {
-            var herb = await _context.Herbs
-                .Include(h => h.Image)
-                .FirstOrDefaultAsync(h => h.Id == id);
-
-            if (herb != null)
-            {
-                _context.Herbs.Remove(herb);
-                await _context.SaveChangesAsync();
-            }
-
-            return RedirectToAction(nameof(Index));
-        }
 
 
-
-
-        // GET: Herbs/Edit/5
-        //public async Task<IActionResult> Edit(int? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    var herb = await _context.Herbs.FindAsync(id);
-        //    if (herb == null)
-        //    {
-        //        return NotFound();
-        //    }
-        //    return View(herb);
-        //}
-
-        //// POST: Herbs/Edit/5
-        //// To protect from overposting attacks, enable the specific properties you want to bind to.
-        //// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public async Task<IActionResult> Edit(int id, [Bind("Id,PopularName,LatinName,DialectNames,Aroma,Taste,Habitat,Season,IsPoisonous,IsProtected,UsedPart,Benefits,Description")] Herb herb)
-        //{
-        //    if (id != herb.Id)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    if (ModelState.IsValid)
-        //    {
-        //        try
-        //        {
-        //            _context.Update(herb);
-        //            await _context.SaveChangesAsync();
-        //        }
-        //        catch (DbUpdateConcurrencyException)
-        //        {
-        //            if (!HerbExists(herb.Id))
-        //            {
-        //                return NotFound();
-        //            }
-        //            else
-        //            {
-        //                throw;
-        //            }
-        //        }
-        //        return RedirectToAction(nameof(Index));
-        //    }
-        //    return View(herb);
-        //}
-
-        //// GET: Herbs/Delete/5
-        //public async Task<IActionResult> Delete(int? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    var herb = await _context.Herbs
-        //        .FirstOrDefaultAsync(m => m.Id == id);
-        //    if (herb == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    return View(herb);
-        //}
 
         // POST: Herbs/Delete/5
         [HttpPost, ActionName("Delete")]
@@ -332,9 +184,10 @@ namespace BilkoNavigator_.Controllers
             if (herb != null)
             {
                 _context.Herbs.Remove(herb);
+                await _context.SaveChangesAsync();
             }
 
-            await _context.SaveChangesAsync();
+            
             return RedirectToAction(nameof(Index));
         }
 
@@ -342,5 +195,22 @@ namespace BilkoNavigator_.Controllers
         {
             return _context.Herbs.Any(e => e.Id == id);
         }
+
+        // GET: Herbs/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var herb = await _context.Herbs
+                .Include(h => h.Image)
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+            if (herb == null)
+                return NotFound();
+
+            return View(herb);
+        }
+
     }
 }
